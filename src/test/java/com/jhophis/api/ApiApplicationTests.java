9ff28819510c7608999bc4ec38090a623d27c878
João Plínio <joaopliniosiqueira@gmail.com>
2022-05-24 13:10:19 -0300

    projeto iniciado

---
+++ src/test/java/com/jhophis/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhophis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
