9ca5aaff0fd3d7fcb264c6811b011aab2e4252e0
João Plínio <joaopliniosiqueira@gmail.com>
2022-05-24 15:25:52 -0300

    classe user

---
+++ src/main/java/com/jhophis/api/domain/User.java
@@ -0,0 +1,22 @@
+package com.jhophis.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
