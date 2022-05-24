2343ba74f4e646a3aa5fbfbb38f6ac0abd5f9c0f
João Plínio <joaopliniosiqueira@gmail.com>
2022-05-24 16:57:17 -0300

    tratamento de excecao para objeto nao encontrado

---
+++ src/main/java/com/jhophis/api/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.jhophis.api.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/jhophis/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/jhophis/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.jhophis.api.services.impl;
 import com.jhophis.api.domain.User;
 import com.jhophis.api.repositories.UserRepository;
 import com.jhophis.api.services.UserService;
+import com.jhophis.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
     }
 }
