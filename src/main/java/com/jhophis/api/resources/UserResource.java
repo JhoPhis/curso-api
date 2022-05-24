c7b7b22898ee1e74ccaf3a5251778d94db8b1a99
João Plínio <joaopliniosiqueira@gmail.com>
2022-05-24 16:32:32 -0300

    classe user resource findById

--- src/main/java/com/jhophis/api/resources/UserResource.java
+++ src/main/java/com/jhophis/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.jhophis.api.resources;
 
 import com.jhophis.api.domain.User;
+import com.jhophis.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Plinio", "plin@mail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
