616fe838c6b192404c521510c6f74df98343c876
João Plínio <joaopliniosiqueira@gmail.com>
2022-05-25 09:05:06 -0300

    endpoint findAll para user

--- src/main/java/com/jhophis/api/services/UserService.java
+++ src/main/java/com/jhophis/api/services/UserService.java
@@ -2,7 +2,10 @@ package com.jhophis.api.services;
 
 import com.jhophis.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
