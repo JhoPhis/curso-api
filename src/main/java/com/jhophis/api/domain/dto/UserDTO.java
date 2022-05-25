5c1c0d58f6ec35c7754a848f222756aa2b47ff05
João Plínio <joaopliniosiqueira@gmail.com>
2022-05-25 08:47:14 -0300

    retornando dto para user resource

--- src/main/java/com/jhophis/api/domain/dto/UserDTO.java
+++ src/main/java/com/jhophis/api/domain/dto/UserDTO.java
@@ -1,7 +1,9 @@
 package com.jhophis.api.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -9,10 +11,13 @@ import javax.persistence.Column;
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
     private String name;
     private String email;
+
+    @JsonIgnore
     private String password;
 }
